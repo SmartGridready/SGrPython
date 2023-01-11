9be8fe18d34e6c9525f283902ee1fd415dabc78c
zupeuc <daniel@clemap.com>
2023-01-11 13:36:02 +0100

    feat: optimized db search complexity

--- sgr_library/restapi_client_async.py
+++ sgr_library/restapi_client_async.py
@@ -19,13 +19,12 @@ def find_dp(root, fp_name: str, dp_name: str) -> Optional[SgrRestApidataPointTyp
     :param dp_name: The name of the datapoint
     :returns: The datapoint element found in root, if not, returns None.
     """
-    for fp in root.fp_list_element:
-            if fp_name == fp.functional_profile.profile_name:
-                #Secondly we filter the datpoint name
-                for dp in fp.dp_list_element:
-                    if dp_name == dp.data_point.datapoint_name:
-                        return dp
-    return None
+
+    fp = next(filter(lambda x: x.functional_profile.profile_name == fp_name, root.fp_list_element), None)
+    if fp:
+        dp = next(filter(lambda x: x.data_point.datapoint_name == dp_name, fp.dp_list_element), None)
+        if dp:
+            return dp
 
 class RestapiConnect():
     """
@@ -47,23 +46,21 @@ class RestapiConnect():
         config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), private_config)
         parser = configparser.ConfigParser()
         parser.read(config_file_path)
-        user = parser.get('AUTHENTICATION', 'username')
-        password = parser.get('AUTHENTICATION', 'password')
-        self.sensor_id = parser.get('RESSOURCE', 'sensor_id')
+        user = parser.get('AUTHENTICATION', 'username', fallback=None)
+        password = parser.get('AUTHENTICATION', 'password', fallback=None)
+        self.sensor_id = parser.get('RESSOURCE', 'sensor_id', fallback=None)
 
         #TODO this one can be formulated more elegantly with a format() method.
-        data =  {
-                'strategy': 'local',
-                'email': user,
-                'password': password
-                }
+        request_body = str(self.root.rest_apiinterface_desc.rest_apibearer.service_call.request_body)
+        data = json.loads(request_body)
+        data['email'] = user
+        data['password'] = password
         self.data = json.dumps(data)
 
         #authentication url
-        self.method = 'https://'
-        self.base_url = self.root.rest_apiinterface_desc.trsp_srv_rest_uriout_of_box
-        self.request_path = self.root.rest_apiinterface_desc.rest_apibearer.service_call.request_path #/authentication
-        self.authentication_url = self.method + str(self.base_url) + str(self.request_path)
+        self.base_url = str(self.root.rest_apiinterface_desc.trsp_srv_rest_uriout_of_box)
+        request_path = str(self.root.rest_apiinterface_desc.rest_apibearer.service_call.request_path) #/authentication
+        self.authentication_url = 'https://' + self.base_url + request_path 
         print(self.authentication_url)
 
         #headers
@@ -79,7 +76,6 @@ class RestapiConnect():
                 print(f"AUTHENTICATION: {res.status}")
                 response = await res.text()
                 token = jmespath.search('accessToken', json.loads(response))
-                #print(token)
                 self.token = str(token)
             else:
                 print(f"AUTHENTICATION: {res.status}")
@@ -91,19 +87,20 @@ class RestapiConnect():
         request_path = dp.rest_apidata_point[0].rest_service_call.request_path
         url = 'https://' + str(self.base_url) + str(request_path)
         url = url.format(sensor_id=self.sensor_id)
+        #TODO add the queryType into an if statement...
         query = str(dp.rest_apidata_point[0].rest_service_call.response_query.query)
 
-        headers =   {
-                    'Content-type': self.content,
-                    'Accept' : self.content,
-                    'Authorization' : 'Bearer ' + self.token
-                    }
+        # Headers dictionary
+        headers = {header_entry.header_name: header_entry.value for header_entry in dp.rest_apidata_point[0].rest_service_call.request_header.header}     
+        #TODO fix reeplacement of bearerToken variable
+        headers['Authorization'] = 'Bearer ' + self.token
+
         async with self.session.get(url=url, headers=headers) as res:
             print(f"GETVAL: {res.status}")
             response = await res.json()
             response = json.dumps(response)
             value = jmespath.search(query, json.loads(response))
-            print(value)
+            return value
 
 async def test():
     client = RestapiConnect()
