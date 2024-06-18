cec8d265e07acd196b583bacfd5a19f6ff9e535d
robin schoch <20967248+robin-schoch@users.noreply.github.com>
2024-06-18 09:04:56 +0200

    version++

--- specification/setup.py
+++ specification/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SGrSpecificationPythontks4r',
-    version='0.1.10',
+    version='0.1.11',
     packages=find_packages(),
     url='https://github.com/robin-schoch/SGrPython',
     license='',
