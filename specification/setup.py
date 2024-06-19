a6a4b9f4746a894fd6dfcc0fcae69d83393eb098
robin schoch <20967248+robin-schoch@users.noreply.github.com>
2024-06-19 09:44:28 +0200

    added external trigger

---
+++ specification/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SGrSpecificationPythontks4r',
+    version='{{tag}}',
+    packages=find_packages(),
+    url='https://github.com/robin-schoch/SGrPython',
+    license='',
+    author='robin',
+    author_email='',
+    description='',
+    long_description='',
+    long_description_content_type='text/plain',
+)
