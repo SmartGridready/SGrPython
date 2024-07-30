8c2308796554e0f2b9708c8dd23d4e362f8b1f0c
robin schoch <20967248+robin-schoch@users.noreply.github.com>
2024-07-30 20:24:54 +0200

    reverted library

--- specification/setup.py
+++ specification/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='SGrSpecifications', # SGrSpecificationPythontks4r
+    name='SGrSpecificationPythontks4r', # SGrSpecificationPythontks4r
     version='{{tag}}',
     packages=find_packages(),
     install_requires=[],
