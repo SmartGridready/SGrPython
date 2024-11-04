2370dba2a3cd5227d99bc6f71817597abe19c6ee
Matthias Krebs <matthias.krebs@ateleris.ch>
2024-11-04 10:58:06 +0100

    cleaned out old EIDs and examples.

--- commhandler/examples/example_generic.py
+++ commhandler/examples/example_generic.py
@@ -2,48 +2,57 @@ import asyncio
 import os
 from sgr_commhandler.device_builder import DeviceBuilder
 
+
+async def run_test_loop():
+
+    print('start loop')
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    #### Modbus
+
+    # We instantiate a Modbus device interface with a Modbus EID XML file
+    eid_file_modbus = os.path.join(current_dir, 'eid', 'abb_terra_01.xml')
+    ini_file_modbus = os.path.join(current_dir, 'ini', 'abb_terra_01_example.ini')
+    modbus_device = (
+        DeviceBuilder()
+        .eid_path(eid_file_modbus)
+        .properties_path(ini_file_modbus)
+        .build()
+    )
+
+    # We connect to the Modbus device
+    await modbus_device.connect_async()
+
+    await modbus_device.get_values_async()
+
+    #### REST
+
+    # We instantiate a REST device interface with a REST-API EID XML file
+    eid_file_rest = os.path.join(current_dir, 'eid', 'SGr_04_mmmm_dddd_CLEMAPEnergyMonitorEIV0.2.1.xml')
+    ini_file_path = os.path.join(current_dir, 'ini', '')
+
+    # We instanciate a second interface object with a modbusTCP xml.
+
+    # We authentificate the restapi conneciton
+
+    # We create a loop where we request a datapoint with a getval of our restapi
+    # component and a datapoint with a getval of our modbus component.
+    while True:
+        # instanciate modbus component and use getval to get a value back.
+        getval = await modbus_device.getval('CurrentAC', 'CurrentACL1')
+        print(getval)
+
+        # instanciate restapi component
+        value = await restapi_component.getval('ActivePowerAC', 'ActivePowerACtot')
+        print(value)
+
+        await asyncio.sleep(1)
+
+        # you could do the same funciton with a asyncio gather functions if you
+        # want to get the variables "concurrently".
+
 if __name__ == "__main__":
-
-    async def run_test_loop():
-
-        print('start loop')
-
-        # We instanciate one interface object with a modbus xml.
-        interface_file_modbus = 'xml/abb_terra_01.xml'
-        builder = DeviceBuilder()
-        modbus_component = builder \
-                            .eid_path(interface_file_modbus) \
-                            .properties({}) \
-                            .build()
-
-        # We connect to the modbus component. # TODO fix connect thingy
-        await modbus_component.connect_async()
-
-        await modbus_component.get_values_async()
-        # We instanciate an interface object with a restapi xml.
-        config_file_rest = 'config_CLEMAPEnMon_ressource_default.ini'
-        config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), config_file_rest)
-
-        # We instanciate a second interface object with a modbusTCP xml.
-
-        # We authentificate the restapi conneciton
-
-        # We create a loop where we request a datapoint with a getval of our restapi
-        # component and a datapoint with a getval of our modbus component.
-        while True:
-            # instanciate modbus component and use getval to get a value back.
-            getval = await modbus_component.getval('CurrentAC', 'CurrentACL1')
-            print(getval)
-
-            # instanciate restapi component
-            value = await restapi_component.getval('ActivePowerAC', 'ActivePowerACtot')
-            print(value)
-
-            await asyncio.sleep(1)
-
-            # you could do the same funciton with a asyncio gather functions if you
-            # want to get the variables "concurrently".
-
     try:
         asyncio.run(run_test_loop())
     except KeyboardInterrupt:
