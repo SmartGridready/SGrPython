5a0c340401ae00f895009135eeb7fd5536d2f262
Matthias Krebs <matthias.krebs@ateleris.ch>
2025-04-01 17:09:14 +0200

    begin code documentation with docstrings, standard python style. documented the most important apis.

--- commhandler/src/sgr_commhandler/api/data_point_api.py
+++ commhandler/src/sgr_commhandler/api/data_point_api.py
@@ -6,50 +6,99 @@ from sgr_specification.v0.generic import DataDirectionProduct, Units
 
 from sgr_commhandler.api.data_types import DataTypes
 
+"""Defines a generic data type."""
 T = TypeVar('T')
 
 
 class DataPointValidator(Protocol):
-    def validate(self, value: Any) -> bool: ...
+    """
+    Defines an interface for data point validators.
+    """
 
-    def data_type(self) -> DataTypes: ...
+    def validate(self, value: Any) -> bool:
+        ...
+
+    def data_type(self) -> DataTypes:
+        ...
 
     def options(self) -> list[Any] | None:
         return None
 
 
 class DataPointProtocol(Protocol):
-    async def set_val(self, value: Any): ...
+    """
+    Defines an interface for data point protocols.
+    """
 
-    async def get_val(self, skip_cache: bool = False) -> Any: ...
+    async def set_val(self, value: Any):
+        ...
 
-    def name(self) -> tuple[str, str]: ...
+    async def get_val(self, skip_cache: bool = False) -> Any:
+        ...
 
-    def direction(self) -> DataDirectionProduct: ...
+    def name(self) -> tuple[str, str]:
+        ...
 
-    def unit(self) -> Units: ...
+    def direction(self) -> DataDirectionProduct:
+        ...
+
+    def unit(self) -> Units:
+        ...
 
     def can_subscribe(self) -> bool:
         return False
 
     def subscribe(self, fn: Callable[[Any], None]):
-        raise Exception('Unsupported operatioin')
+        raise Exception('Unsupported operation')
 
     def unsubscribe(self):
-        raise Exception('Unsupported operatioin')
+        raise Exception('Unsupported operation')
 
 
 class DataPoint(Generic[T]):
+    """
+    Implements a data point of a generic data type.
+    """
+
     def __init__(
         self, protocol: DataPointProtocol, validator: DataPointValidator
     ):
+        """
+        Constructs a data point.
+
+        Parameters
+        ----------
+        protocol : DataPointProtocol
+            the underlying protocol
+        validator : DataPointValidator
+            the data point's value validator
+        """
+
         self._protocol = protocol
         self._validator = validator
 
     def name(self) -> tuple[str, str]:
+        """
+        Gets the data point name.
+
+        Returns
+        -------
+        tuple[str, str]
+            the functional profile and data point name
+        """
+
         return self._protocol.name()
 
     async def get_value_async(self) -> T:
+        """
+        Gets the data point value asynchronously.
+
+        Returns
+        -------
+        T
+            the data point value
+        """
+
         value = await self._protocol.get_val()
         if self._validator.validate(value):
             return value
@@ -58,26 +107,84 @@ class DataPoint(Generic[T]):
         )
 
     def get_value(self) -> T:
+        """
+        Gets the data point value synchronously.
+
+        Returns
+        -------
+        T
+            the data point value
+        """
+
         return run(self.get_value_async())
 
     async def set_value_async(self, value: T):
+        """
+        Sets the data point value asynchronously.
+
+        Parameters
+        ----------
+        value: T
+            the data point value
+        """
+
         if self._validator.validate(value):
             return await self._protocol.set_val(value)
         raise Exception('invalid data to write to device')
 
     def set_value(self, value: T):
+        """
+        Sets the data point value synchronously.
+
+        Parameters
+        ----------
+        value: T
+            the data point value
+        """
+
         return run(self.set_value_async(value))
 
     def subscribe(self, fn: Callable[[Any], None]):
+        """
+        Subscribes to data point value changes.
+
+        Parameters
+        ----------
+        fn : Callable[[Any], None]
+            the handler method
+        """
+
         self._protocol.subscribe(fn)
 
     def unsubscribe(self):
+        """
+        Unsubscribes from data point value changes.
+        """
+
         self._protocol.unsubscribe()
 
     def direction(self) -> DataDirectionProduct:
+        """
+        Gets the data point direction.
+
+        Returns
+        -------
+        DataDirectionProduct
+            the data point direction
+        """
+
         return self._protocol.direction()
 
     def data_type(self) -> DataTypes:
+        """
+        Gets the data point data type.
+
+        Returns
+        -------
+        DataTypes
+            the data point data type
+        """
+
         return self._validator.data_type()
     
     def unit(self) -> Units:
@@ -86,9 +193,27 @@ class DataPoint(Generic[T]):
     def describe(
         self,
     ) -> tuple[tuple[str, str], DataDirectionProduct, DataTypes, Units]:
+        """
+        Describes the data point.
+
+        Returns
+        -------
+        tuple[tuple[str, str], DataDirectionProduct, DataTypes, Units]
+            the data point information
+        """
+
         return self.name(), self.direction(), self.data_type(), self.unit()
 
     def options(self) -> list[Any]:
+        """
+        Describes the data point options.
+
+        Returns
+        -------
+        list[Any]
+            the data point options
+        """
+
         options = self._validator.options()
         if options is None:
             return []
