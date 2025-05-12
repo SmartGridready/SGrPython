ceda78438d4badbc60f3577c1c6d90a1338463a5
Matthias Krebs <matthias.krebs@ateleris.ch>
2025-05-12 10:47:39 +0200

    migrated all build config to pyproject.toml, except version.

--- commhandler/setup.py
+++ commhandler/setup.py
@@ -1,18 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sgr-commhandler",
     version="0.4.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    url="https://github.com/SmartGridready/SGrPython/commhandler#readme",
-    project_urls={
-        "Issue Tracker": "https://github.com/SmartGridready/SGrPython/issues",
-        "Source": "https://github.com/SmartGridready/SGrPython",
-        "Docs": "https://smartgridready.github.io/SGrPython/docs/commhandler/"
-    },
-    author="Verein SmartGridready",
-    description="SGr Commhandler",
-    long_description="Communication Handler Library",
-    long_description_content_type="text/plain",
 )
--- specification/setup.py
+++ specification/setup.py
@@ -1,18 +1,5 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="sgr-specification",
     version="2.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    url="https://github.com/SmartGridready/SGrPython/specification#readme",
-    project_urls={
-        "Issue Tracker": "https://github.com/SmartGridready/SGrSpecifications/issues",
-        "Source": "https://github.com/SmartGridready/SGrSpecifications",
-        "Docs": "https://smartgridready.github.io/"
-    },
-    author="Verein SmartGridready",
-    description="SGr Specification",
-    long_description="Specification classes generated from XML schema",
-    long_description_content_type="text/plain",
 )
