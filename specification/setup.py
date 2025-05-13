79ad103e4773e8f73aea6131aa7f92a371513b4d
Matthias Krebs <58782488+mkrebs81@users.noreply.github.com>
2025-05-13 10:01:05 +0200

    fixed format of pyproject.toml (#69)
    * fixed format of pyproject.toml. build process should no longer fail.
    * removed setuptools dependency in favor of 'build' tool.
    * migrated all build config to pyproject.toml, except version.

--- specification/setup.py
+++ specification/setup.py
@@ -1,22 +1,5 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="sgr-specification",
     version="2.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=[],
-    url="https://github.com/SmartGridready/SGrPython/specification#readme",
-    project_urls={
-        "Issue Tracker": "https://github.com/SmartGridready/SGrSpecifications/issues",
-        "Source": "https://github.com/SmartGridready/SGrSpecifications",
-        "Docs": "https://smartgridready.github.io/"
-    },
-    license="BSD-3",
-    license_files=["LICENSE.md"],
-    author="Verein SmartGridready",
-    description="SGr Specification",
-    long_description="Specification classes generated from XML schema",
-    long_description_content_type="text/plain",
-    python_requires=">=3.9",
 )
