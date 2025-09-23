cad380c65a980031d6fa2af80357f6277da02eec
Matthias Krebs <matthias.krebs@ateleris.ch>
2025-09-23 10:02:45 +0200

    added jmespath test.

--- commhandler/tests/test_utils/test_jmespath.py
+++ commhandler/tests/test_utils/test_jmespath.py
@@ -11,7 +11,7 @@ from sgr_commhandler.utils import (
 Test JMESPath mapping.
 """
 
-def test_jmespath_mapping():
+def test_jmespath_mapping_1():
     jmes_mappings = [
         JmespathMappingRecord(from_value="[*].start_timestamp", to="[*].start_timestamp"),
         JmespathMappingRecord(from_value="[*].end_timestamp", to="[*].end_timestamp"),
@@ -61,3 +61,85 @@ def test_jmespath_mapping():
 
     json_mapped = jmespath_mapping.map_json_response(json_input, jmes_mappings)
     assert json_mapped == json_expected
+
+
+def test_jmespath_mapping_2():
+    jmes_mappings = [
+        JmespathMappingRecord(from_value="prices[*].start_timestamp", to="[*].start_timestamp"),
+        JmespathMappingRecord(from_value="prices[*].end_timestamp", to="[*].end_timestamp"),
+        #JmespathMappingRecord(from_value="prices[*].integrated[*].value", to="[*].integrated[*].value"),
+        #JmespathMappingRecord(from_value="prices[*].integrated[*].unit", to="[*].integrated[*].unit"),
+        #JmespathMappingRecord(from_value="prices[*].integrated[*].component", to="[*].integrated[*].component")
+        JmespathMappingRecord(from_value="prices[*].*[*].value", to="[*].*[*].value"),
+        JmespathMappingRecord(from_value="prices[*].*[*].unit", to="[*].*[*].unit"),
+        JmespathMappingRecord(from_value="prices[*].*[*].component", to="[*].*[*].component")
+    ]
+    json_input = json.dumps({
+        "status": "ok",
+        "prices": [
+            {
+                "start_timestamp": "2025-01-01T00:00:00+01:00",
+                "end_timestamp": "2025-01-01T00:15:00+01:00",
+                "integrated": [
+                    {
+                        "value": 4.7,
+                        "unit": "CHF/kWh",
+                        "component": "work"
+                    }
+                ],
+                "grid": [
+                    {
+                        "value": 1.7,
+                        "unit": "CHF/kWh",
+                        "component": "work"
+                    }
+                ]
+            },
+            {
+                "start_timestamp": "2025-01-01T00:15:00+01:00",
+                "end_timestamp": "2025-01-01T00:30:00+01:00",
+                "integrated": [
+                    {
+                        "value": 4.698633,
+                        "unit": "CHF/kWh",
+                        "component": "work"
+                    }
+                ],
+                "grid": [
+                    {
+                        "value": 2.7,
+                        "unit": "CHF/kWh",
+                        "component": "work"
+                    }
+                ]
+            }
+        ]
+    })
+    json_expected = json.dumps([
+        {
+            "start_timestamp": "2025-01-01T00:00:00+01:00",
+            "end_timestamp": "2025-01-01T00:15:00+01:00",
+            "integrated": [
+                {
+                    "value": 4.7,
+                    "unit": "CHF/kWh",
+                    "component": "work"
+                }
+            ]
+        },
+        {
+            "start_timestamp": "2025-01-01T00:15:00+01:00",
+            "end_timestamp": "2025-01-01T00:30:00+01:00",
+            "integrated": [
+                {
+                    "value": 4.698633,
+                    "unit": "CHF/kWh",
+                    "component": "work"
+                }
+            ]
+        }
+    ])
+
+    json_mapped = jmespath_mapping.map_json_response(json_input, jmes_mappings)
+    assert json_mapped == json_expected
+
