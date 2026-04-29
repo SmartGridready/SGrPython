0b48831209b271b095e00659603455037e20fb75
Matthias Krebs <matthias.krebs@ateleris.ch>
2026-04-29 10:58:59 +0200

    bumped docs version. constructor fix of RestApiServiceCall.

--- commhandler/docs/conf.py
+++ commhandler/docs/conf.py
@@ -7,11 +7,11 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SGr CommHandler'
-copyright = '2025, Verein SmartGridready'
+copyright = '2026, Verein SmartGridready'
 author = 'Matthias Krebs'
 
-release = '0.4'
-version = '0.4.0'
+release = '0.5'
+version = '0.5.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
